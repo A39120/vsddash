15dba11c75dacc3074ad6d1b1c9b3757b9cb698e
A39120 <lariachi@gmail.com>
2019-03-13 21:18:18 +0000

    Initiated Server.

---
+++ server/src/main/java/pt/isel/vsdserver/VsdserverApplication.java
@@ -0,0 +1,13 @@
+package pt.isel.vsdserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsdserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VsdserverApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/pt/isel/vsdserver/VsdserverApplicationTests.java
@@ -0,0 +1,16 @@
+package pt.isel.vsdserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VsdserverApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
