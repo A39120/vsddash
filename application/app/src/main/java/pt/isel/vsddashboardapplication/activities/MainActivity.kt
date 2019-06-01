6cbde09f3f337ef480fcf21c913f882d16ac35b0
A39120 <lariachi@gmail.com>
2019-06-02 00:48:15 +0100

    Cleaned Main Activity.

--- application/app/src/main/java/pt/isel/vsddashboardapplication/activities/MainActivity.kt
+++ application/app/src/main/java/pt/isel/vsddashboardapplication/activities/MainActivity.kt
@@ -2,38 +2,14 @@ package pt.isel.vsddashboardapplication.activities
 
 import android.os.Bundle
 import pt.isel.vsddashboardapplication.activities.base.BaseActivity
-import androidx.fragment.app.FragmentManager
 import pt.isel.vsddashboardapplication.R
-import androidx.fragment.app.Fragment
 
 class MainActivity : BaseActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
         setContentView(R.layout.main_activity)
-        //binding.lifecycleOwner = this
-
-        /*addFragmentToActivity(
-            this.supportFragmentManager,
-            ApiSettingsFragment(),
-            R.id.container
-        )*/
-
-        addFragmentToActivity(
-            this.supportFragmentManager,
-            NSGatewayListFragment(),
-            R.id.container
-        )
-
-    }
-
-    fun addFragmentToActivity(manager: FragmentManager, fragment: Fragment, frameId: Int) {
-        manager.beginTransaction().run {
-            add(frameId, fragment)
-            commit()
-        }
     }
 
 }
