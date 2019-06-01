1e0422b09ec52eb15da9f41363b90889c6f9afbf
A39120 <lariachi@gmail.com>
2019-06-02 00:43:01 +0100

    More NSG

--- application/app/src/main/java/pt/isel/vsddashboardapplication/activities/NSGatewayFragment.kt
+++ application/app/src/main/java/pt/isel/vsddashboardapplication/activities/NSGatewayFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import pt.isel.vsddashboardapplication.R
+import pt.isel.vsddashboardapplication.viewmodel.NsgatewayViewModel
+import pt.isel.vsddashboardapplication.databinding.NsgatewayFragmentBinding
 
 class NSGatewayFragment : Fragment() {
 
@@ -16,18 +19,27 @@ class NSGatewayFragment : Fragment() {
     }
 
     private lateinit var viewModel: NsgatewayViewModel
+    private lateinit var binding: NsgatewayFragmentBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+       super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(NsgatewayViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.nsgateway_fragment, container, false)
-    }
+        binding = DataBindingUtil.inflate<NsgatewayFragmentBinding>(
+            inflater,
+            R.layout.nsgateway_fragment,
+            container,
+            false
+        )
+        binding.viewmodel = viewModel
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(NsgatewayViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        return binding.root
     }
 
 }
